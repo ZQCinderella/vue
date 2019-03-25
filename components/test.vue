3c7decb78989d9e0dce5a0b6f50b598cce56591d
ZQCinderella <604017846@qq.com>
2019-03-25 19:00:15 +0800

    增加插槽的使用方法

--- components/test.vue
+++ components/test.vue
@@ -3,7 +3,8 @@
         <!-- 假如路由是/test/name, 则params.pathMatch === /name -->
         这是测试组件，动态路由为{{$route.path}}, 匹配路由为{{$route.params.pathMatch}}
         <slot-com>
-            <div slot="view-container">我是具名插槽, header: font-size:30px</div>
+            <!-- 需要注意的是，插槽只能访问被调用组件，即父组件中的变量和方法 -->
+            <div :slot="slotName">我是具名插槽, header: font-size:30px</div>
             <div>我是默认插槽 footer: font-size: 25px</div>
         </slot-com>
         <button @click="showModal">show modal</button>
@@ -12,6 +13,11 @@
 <script>
 import slotCom from "./slot.vue"
     export default {
+        data() {
+            return {
+                slotName: 'view-container'
+            }
+        },
         components: {
             slotCom
         },
